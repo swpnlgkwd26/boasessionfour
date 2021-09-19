1679ce606ce075b399424bfa1ad4039cbd0256cb
Swapnil Gaikwad <swpnlgkwd@hotmail.com>
2021-09-19 13:18:29 +0530

    Day8 Blazor , Binding Events Templated Component Splatting,  Key  , Show Products

---
+++ BoaFourSolution/BlazorServerappDemo/Data/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerappDemo.Data
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProducts();
+        Task<Product> GetProductById(int productID);
+    }
+}
---
+++ BoaFourSolution/BlazorServerappDemo/Data/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerappDemo.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDBContext _context;
+
+        public ProductRepository(ProductDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public Task<List<Product>> GetProducts()
+        {
+            return Task.FromResult(_context.Products.ToList());
+        }
+    }
+}
